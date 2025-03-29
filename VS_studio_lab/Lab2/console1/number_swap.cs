77287acb1cb5400fd08ad2abd4dc2590eeefde8f
Atserphs <rabinthimi556@gmail.com>
2025-03-29 17:30:36 +0545

    CommitOn - Lab2 Folder - Lab2 Content Added

---
+++ VS_studio_lab/Lab2/console1/number_swap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace console1
+{
+    class number_swap
+    {
+        public void number_swap_func()
+        {
+            int a = 18;
+            int b = 6;
+            int c;
+
+            Console.WriteLine("\nValues before swap:\n a = " + a + " b = " + b);
+            c = b;
+            b = a;
+            a = c;
+            Console.WriteLine("Values after swap:\n a = " + a + " b = " + b);
+        }
+    }
+}
---
+++ VS_studio_lab/Lab2/console1/sum_n_number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1
+{
+    class sum_n_number
+    {
+        public int sum_n_number_func(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i<=n; i++)
+            {
+                sum = sum + i;    
+            }
+            return sum;
+        }
+
+        public int sum_n_number_func1(int n)
+        {
+            int sum1 = n * (n + 1) / 2;
+            return sum1;
+        }
+    }
+}
