707d94ead998069d5a6cdf6d713fc9afbfb67acb
Atserphs <rabinthimi556@gmail.com>
2025-05-05 18:29:40 +0545

    CommitOn - Lab4 - Lab4 WinForm code is uploaded

--- VS_studio_lab/Lab2/console1/obj/Release/net8.0/console1.AssemblyInfo.cs
+++ VS_studio_lab/Lab2/console1/obj/Release/net8.0/console1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("console1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77287acb1cb5400fd08ad2abd4dc2590eeefde8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c43a1eedb9b0998990809f702992954e12f2c301")]
 [assembly: System.Reflection.AssemblyProductAttribute("console1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("console1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
